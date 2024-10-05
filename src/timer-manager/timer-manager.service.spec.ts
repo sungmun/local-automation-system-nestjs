77d0e2c99a1abb7118dbdb9b9c53c24fb7536b58
sungmun <tjdans174@mealant.com>
2024-10-05 14:26:21 +0900

    test: 타이머 매니저 서비스 테스트 코드 작성

--- src/timer-manager/timer-manager.service.spec.ts
+++ src/timer-manager/timer-manager.service.spec.ts
@@ -12,7 +12,35 @@ describe('TimerManagerService', () => {
     service = module.get<TimerManagerService>(TimerManagerService);
   });
 
-  it('should be defined', () => {
+  it('서비스가 정의되어야 한다', () => {
     expect(service).toBeDefined();
+  });
+
+  it('타이머를 설정해야 한다', () => {
+    const callback = jest.fn();
+    service.setTimer('test', callback, 1000);
+    expect(service['timers'].has('test')).toBe(true);
+  });
+
+  it('키가 이미 존재하면 타이머를 설정하지 않아야 한다', () => {
+    const callback = jest.fn();
+    service.setTimer('test', callback, 1000);
+    service.setTimer('test', callback, 1000);
+    expect(service['timers'].size).toBe(1);
+  });
+
+  it('타이머를 제거해야 한다', () => {
+    const callback = jest.fn();
+    service.setTimer('test', callback, 1000);
+    service.clearTimer('test');
+    expect(service['timers'].has('test')).toBe(false);
+  });
+
+  it('지연 후 콜백을 호출해야 한다', (done) => {
+    const callback = jest.fn(() => {
+      expect(callback).toHaveBeenCalled();
+      done();
+    });
+    service.setTimer('test', callback, 100);
   });
 });
