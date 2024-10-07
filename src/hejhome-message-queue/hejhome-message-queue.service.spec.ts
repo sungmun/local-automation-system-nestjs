e7de74f3123f80d584bd68a43117df4a77a24176
sungmun <tjdans174@mealant.com>
2024-10-07 10:27:12 +0900

    test: hejhome 메세지 큐 테스트 코드 추가

--- src/hejhome-message-queue/hejhome-message-queue.service.spec.ts
+++ src/hejhome-message-queue/hejhome-message-queue.service.spec.ts
@@ -4,6 +4,7 @@ import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 
 describe('HejhomeMessageQueueService', () => {
   let service: HejhomeMessageQueueService;
+  let amqpConnection: AmqpConnection;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,9 +22,20 @@ describe('HejhomeMessageQueueService', () => {
     service = module.get<HejhomeMessageQueueService>(
       HejhomeMessageQueueService,
     );
+    amqpConnection = module.get<AmqpConnection>(AmqpConnection);
   });
 
-  it('should be defined', () => {
+  it('서비스가 정의되어야 한다', () => {
     expect(service).toBeDefined();
+  });
+
+  it('연결되어 있을 때 true를 반환해야 한다', () => {
+    (amqpConnection as any)['connected'] = true;
+    expect(service.isConnected()).toBe(true);
+  });
+
+  it('연결되어 있지 않을 때 false를 반환해야 한다', () => {
+    (amqpConnection as any)['connected'] = false;
+    expect(service.isConnected()).toBe(false);
   });
 });
