bd869c781ded6868e5d8105135dd8ea40971d348
sungmun <tjdans174@mealant.com>
2024-10-27 10:24:37 +0900

    feat: DeviceLog entity 정의
    Resolves: #50

--- src/device-log/entities/device-log.entity.ts
+++ src/device-log/entities/device-log.entity.ts
@@ -1,1 +1,24 @@
-export class DeviceLog {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class DeviceLog {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  deviceId: string;
+
+  @Column('text')
+  logMessage: string;
+
+  @Column()
+  type: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
