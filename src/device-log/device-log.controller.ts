7625bd43d6f3e67103e2da19db8b70066f316d13
sungmun <tjdans174@mealant.com>
2024-10-27 20:29:24 +0900

    fix : 사용하지 않는 데코레이터 제거및 함수 제거
    Resolves: #50

--- src/device-log/device-log.controller.ts
+++ src/device-log/device-log.controller.ts
@@ -1,35 +1,7 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
 import { DeviceLogService } from './device-log.service';
-import { CreateDeviceLogDto } from './dto/create-device-log.dto';
-import { UpdateDeviceLogDto } from './dto/update-device-log.dto';
 
 @Controller()
 export class DeviceLogController {
   constructor(private readonly deviceLogService: DeviceLogService) {}
-
-  @MessagePattern('createDeviceLog')
-  create(@Payload() createDeviceLogDto: CreateDeviceLogDto) {
-    return this.deviceLogService.create(createDeviceLogDto);
-  }
-
-  @MessagePattern('findAllDeviceLog')
-  findAll() {
-    return this.deviceLogService.findAll();
-  }
-
-  @MessagePattern('findOneDeviceLog')
-  findOne(@Payload() id: number) {
-    return this.deviceLogService.findOne(id);
-  }
-
-  @MessagePattern('updateDeviceLog')
-  update(@Payload() updateDeviceLogDto: UpdateDeviceLogDto) {
-    return this.deviceLogService.update(updateDeviceLogDto.id, updateDeviceLogDto);
-  }
-
-  @MessagePattern('removeDeviceLog')
-  remove(@Payload() id: number) {
-    return this.deviceLogService.remove(id);
-  }
 }
