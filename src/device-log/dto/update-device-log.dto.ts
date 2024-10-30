1981ebe5df51d66a139fbfe1d3b14f24b86980b3
sungmun <tjdans174@mealant.com>
2024-10-30 18:48:26 +0900

    feat: 사용하지 않는 dto 제거
    Resolves: #50

--- src/device-log/dto/update-device-log.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDeviceLogDto } from './create-device-log.dto';
-
-export class UpdateDeviceLogDto extends PartialType(CreateDeviceLogDto) {
-  id: number;
-}
