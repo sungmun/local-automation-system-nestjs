7f52a085d1db4020fe8d5c9efa0a3e53bd9e0b7f
sungmun <tjdans174@mealant.com>
2024-10-30 18:48:57 +0900

    CreateDeviceLogDto 테스트 코드 추가
    - 검증로직이 없기떄문에 정의가 되었는지만 검증한다
    
    Resolves: #50

---
+++ src/device-log/dto/create-device-log.dto.spec.ts
@@ -0,0 +1,7 @@
+import { CreateDeviceLogDto } from './create-device-log.dto';
+
+describe('CreateDeviceLogDto', () => {
+  it('CreateDeviceLogDto가 정의되어야 한다', () => {
+    expect(CreateDeviceLogDto).toBeDefined();
+  });
+});
