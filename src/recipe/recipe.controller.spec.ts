fc231bba23cfefdcd6068d05d3acb0c49926f300
sungmun <tjdans174@mealant.com>
2024-11-09 14:35:58 +0900

    feat: 코드가 동작할 정도의 테스트 코드 수정
    Resolves: #71

--- src/recipe/recipe.controller.spec.ts
+++ src/recipe/recipe.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecipeController } from './recipe.controller';
 import { RecipeService } from './recipe.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Recipe } from './entities/recipe.entity';
+import { DeviceCommand } from './entities/device-command.entity';
 
 describe('RecipeController', () => {
   let controller: RecipeController;
@@ -8,7 +11,17 @@ describe('RecipeController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RecipeController],
-      providers: [RecipeService],
+      providers: [
+        RecipeService,
+        {
+          provide: getRepositoryToken(Recipe),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(DeviceCommand),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<RecipeController>(RecipeController);
--- src/recipe/recipe.service.spec.ts
+++ src/recipe/recipe.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecipeService } from './recipe.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Recipe } from './entities/recipe.entity';
+import { DeviceCommand } from './entities/device-command.entity';
 
 describe('RecipeService', () => {
   let service: RecipeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RecipeService],
+      providers: [
+        RecipeService,
+        {
+          provide: getRepositoryToken(Recipe),
+          useValue: {
+            save: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(DeviceCommand),
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<RecipeService>(RecipeService);
