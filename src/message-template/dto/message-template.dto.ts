d7295fe0b4104975f586ef021a801b2fc578308c
sungmun <tjdans174@mealant.com>
2024-12-07 23:34:01 +0900

    feat: message-template swagger 관련 데코레이터 추가
    Resolves: #66

--- src/message-template/dto/message-template.dto.ts
+++ src/message-template/dto/message-template.dto.ts
@@ -1,6 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class MessageTemplateDto {
+  @ApiProperty({
+    description: '메시지 템플릿 이름',
+    example: '알림 템플릿',
+  })
   name: string;
+
+  @ApiProperty({
+    description: '메시지 템플릿 본문',
+    example: '알림 템플릿 본문',
+  })
   body: string;
+
+  @ApiProperty({
+    description: '메시지 템플릿 제목',
+    example: '알림 템플릿 제목',
+  })
   title: string;
+
+  @ApiProperty({
+    description: '메시지 템플릿 타입',
+    example: 'ALARM',
+  })
   type: string;
 }
--- src/message-template/message-template.controller.ts
+++ src/message-template/message-template.controller.ts
@@ -19,13 +19,26 @@ import {
   CreateMessageTemplateResponseDto,
   ListMessageTemplateResponseDto,
 } from './dto/response';
+import {
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('메시지 템플릿')
 @Controller('message-templates')
 export class MessageTemplateController {
   constructor(
     private readonly messageTemplateService: MessageTemplateService,
   ) {}
 
+  @ApiOperation({ summary: '메시지 템플릿 생성' })
+  @ApiCreatedResponse({
+    description: '메시지 템플릿이 생성되었습니다.',
+    type: CreateMessageTemplateResponseDto,
+  })
   @Post()
   @HttpCode(HttpStatus.CREATED)
   async create(
@@ -37,6 +50,11 @@ export class MessageTemplateController {
     return plainToInstance(CreateMessageTemplateResponseDto, result);
   }
 
+  @ApiOperation({ summary: '메시지 템플릿 목록 조회' })
+  @ApiOkResponse({
+    description: '메시지 템플릿 목록을 조회했습니다.',
+    type: ListMessageTemplateResponseDto,
+  })
   @Get()
   @HttpCode(HttpStatus.OK)
   async findAll() {
@@ -44,6 +62,11 @@ export class MessageTemplateController {
     return plainToInstance(ListMessageTemplateResponseDto, { list: result });
   }
 
+  @ApiOperation({ summary: '메시지 템플릿 상세 조회' })
+  @ApiOkResponse({
+    description: '메시지 템플릿을 조회했습니다.',
+    type: DetailMessageTemplateResponseDto,
+  })
   @Get(':messageTemplateId')
   @HttpCode(HttpStatus.OK)
   async findOne(@Param('messageTemplateId') id: string) {
@@ -51,6 +74,10 @@ export class MessageTemplateController {
     return plainToInstance(DetailMessageTemplateResponseDto, result);
   }
 
+  @ApiOperation({ summary: '메시지 템플릿 수정' })
+  @ApiNoContentResponse({
+    description: '메시지 템플릿이 수정되었습니다.',
+  })
   @Patch(':messageTemplateId')
   @HttpCode(HttpStatus.NO_CONTENT)
   async update(
