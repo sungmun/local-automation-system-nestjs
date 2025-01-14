cf793f535d09d403455daad59fc4e9b940e8d369
sungmun <tjdans174@mealant.com>
2025-01-14 14:09:56 +0900

    feat: ConfigService 통합으로 HejhomeMessageQueueService 개선
    - 더 나은 구성 관리를 위해 ConfigService를 포함하도록 HejhomeMessageQueueService를 업데이트했습니다.
    - 구성을 통해 메시지 큐가 비활성화된 경우 false를 반환하도록 isConnected 메서드를 수정했습니다.
    - 구성 설정에 따라 새로운 동작을 다루기 위해 HejhomeMessageQueueService에 대한 단위 테스트를 개선했습니다.
    
    이 변경은 구성 변경에 대한 서비스의 유연성과 응답성을 개선합니다.

--- src/hejhome-message-queue/hejhome-message-queue.service.spec.ts
+++ src/hejhome-message-queue/hejhome-message-queue.service.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HejhomeMessageQueueService } from './hejhome-message-queue.service';
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+import { ConfigService } from '@nestjs/config';
 
 describe('HejhomeMessageQueueService', () => {
   let service: HejhomeMessageQueueService;
   let amqpConnection: AmqpConnection;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,6 +18,12 @@ describe('HejhomeMessageQueueService', () => {
             connected: true,
           },
         },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockReturnValue('true'),
+          },
+        },
       ],
     }).compile();
 
@@ -23,19 +31,27 @@ describe('HejhomeMessageQueueService', () => {
       HejhomeMessageQueueService,
     );
     amqpConnection = module.get<AmqpConnection>(AmqpConnection);
+    configService = module.get<ConfigService>(ConfigService);
   });
 
   it('서비스가 정의되어야 한다', () => {
     expect(service).toBeDefined();
   });
 
-  it('연결되어 있을 때 true를 반환해야 한다', () => {
-    (amqpConnection as any)['connected'] = true;
-    expect(service.isConnected()).toBe(true);
-  });
+  describe('isConnected', () => {
+    it('MQ가 비활성화되어 있으면 false를 반환해야 한다', () => {
+      jest.spyOn(configService, 'get').mockReturnValue('false');
+      expect(service.isConnected()).toBe(false);
+    });
 
-  it('연결되어 있지 않을 때 false를 반환해야 한다', () => {
-    (amqpConnection as any)['connected'] = false;
-    expect(service.isConnected()).toBe(false);
+    it('MQ가 활성화되어 있고 연결되어 있으면 true를 반환해야 한다', () => {
+      (amqpConnection as any)['connected'] = true;
+      expect(service.isConnected()).toBe(true);
+    });
+
+    it('MQ가 활성화되어 있지만 연결되어 있지 않으면 false를 반환해야 한다', () => {
+      (amqpConnection as any)['connected'] = false;
+      expect(service.isConnected()).toBe(false);
+    });
   });
 });
